fa7f5ee87c20808189eba6f98a0a503789f602b4
Miha-x64 <silizium@ok.de>
2020-03-06 01:07:13 +0300

    fixed typo

--- app/src/main/java/net/aquadc/tgchart/DoubleEvaluator.java
+++ app/src/main/java/net/aquadc/tgchart/DoubleEvaluator.java
@@ -11,7 +11,7 @@ public final class DoubleEvaluator implements TypeEvaluator<Double> {
 
     @Override public Double evaluate(float fraction, Double startValue, Double endValue) {
         if (startValue.isNaN() || startValue.isInfinite() || endValue.isNaN() || endValue.isInfinite()) {
-            throw new AssertionError("can't evaluate [" + startValue + "; " + endValue + "], git invalid values");
+            throw new AssertionError("can't evaluate [" + startValue + "; " + endValue + "], got invalid values");
         }
         double diff = endValue - startValue;
         return startValue + fraction * diff;
